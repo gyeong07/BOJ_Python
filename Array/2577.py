797ef5a00d3618a48c8e8feff4500e40330b4103
gyeong07 <vlem4646@gmail.com>
2021-08-17 10:47:00 +0900

    2577_숫자의 개수

---
+++ Array/2577.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+result = []
+result = str(a*b*c)
+
+print(result.count("0"))
+print(result.count("1"))
+print(result.count("2"))
+print(result.count("3"))
+print(result.count("4"))
+print(result.count("5"))
+print(result.count("6"))
+print(result.count("7"))
+print(result.count("8"))
+print(result.count("9"))
